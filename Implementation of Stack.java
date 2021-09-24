25b9031a325c94ce8f7a52ba0e7f3e5b9135a662
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-09-24 11:19:30 +0530

    Create Implementation of Stack.java

---
+++ Implementation of Stack.java
@@ -0,0 +1,12 @@
+private ListNode top;
+	private int length;
+	
+	private class ListNode {
+		private int data; // Can be a generic type
+		private ListNode next; // Reference to next ListNode in list
+		
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
